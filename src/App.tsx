cbcec54622ff4a47e213561873f954e8e7bc592e
Mary Karakida <makarakida@gmail.com>
2022-09-30 09:15:16 -0300

    feat: set up react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import AppRoutes from './routes';
 
 function App() {
-    return <h1>This is app</h1>;
+    return (
+        <BrowserRouter>
+            <AppRoutes />
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export {};
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<h1>This is Home Page</h1>} />
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
+}
+
+export default AppRoutes;
