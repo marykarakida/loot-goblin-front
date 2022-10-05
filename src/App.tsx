38d90130289a6a4c1c2ba6e66c3b78292d586a07
Mary Karakida <makarakida@gmail.com>
2022-10-05 19:51:39 -0300

    feat: config react dnd for desktop and mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
-import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
+import { DndProvider } from 'react-dnd';
 
 import { AuthProvider } from './shared/contexts';
 
 import AppRoutes from './routes';
 
+const isTouchDevice = !!('ontouchstart' in window || navigator.maxTouchPoints);
+
 function App() {
     return (
         <AuthProvider>
-            <DndProvider backend={HTML5Backend}>
+            <DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend} options={{ enableMouseEvents: true }}>
                 <BrowserRouter>
                     <AppRoutes />
                 </BrowserRouter>
